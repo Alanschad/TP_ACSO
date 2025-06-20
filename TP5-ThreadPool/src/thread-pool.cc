3a8e752e178ef5bffc6b105c1b207a59be70ed0a
Alanschad <126919201+Alanschad@users.noreply.github.com>
2025-06-20 02:12:16 -0300

    Subo Tp Casi terminado
    tp que pasa todos los test

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -1,17 +1,71 @@
-/**
- * File: thread-pool.cc
- * --------------------
- * Presents the implementation of the ThreadPool class.
- */
-
 #include "thread-pool.h"
-using namespace std;
+#include <stdexcept>
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {}
+ThreadPool::ThreadPool(size_t numThreads)
+    : numThreads(numThreads), workingThreads(0), shuttingDown(false) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers.emplace_back([this]() { workerLoop(); });
+    }
+}
 
+void ThreadPool::schedule(const function<void(void)>& thunk) {
+    if (!thunk) {
+        throw std::invalid_argument("Cannot schedule empty task");
+    }
+    unique_lock<mutex> lock(queueLock);
+    if (shuttingDown) {
+        throw runtime_error("ThreadPool has been destroyed or is shutting down");
+    }
+    taskQueue.push(thunk);
+    cv_task.notify_one();
+}
 
-void ThreadPool::schedule(const function<void(void)>& thunk) {}
+void ThreadPool::workerLoop() {
+    while (true) {
+        function<void(void)> task;
+        {
+            unique_lock<mutex> lock(queueLock);
+            cv_task.wait(lock, [this]() {
+                return shuttingDown || !taskQueue.empty();
+            });
 
-void ThreadPool::wait() {}
+            if (shuttingDown && taskQueue.empty()) return;
 
-ThreadPool::~ThreadPool() {}
+            task = taskQueue.front();
+            taskQueue.pop();
+            ++workingThreads;
+        }
+
+        if (task) {
+            task(); 
+        } else {
+            throw std::runtime_error("Scheduled empty task executed");
+        }
+
+        {
+            unique_lock<mutex> lock(queueLock);
+            --workingThreads;
+            if (taskQueue.empty() && workingThreads == 0) {
+                cv_wait.notify_all();
+            }
+        }
+    }
+}
+
+void ThreadPool::wait() {
+    unique_lock<mutex> lock(queueLock);
+    cv_wait.wait(lock, [this]() {
+        return taskQueue.empty() && workingThreads == 0;
+    });
+}
+
+ThreadPool::~ThreadPool() {
+    {
+        unique_lock<mutex> lock(queueLock);
+        shuttingDown = true;
+        cv_task.notify_all();
+    }
+    for (thread &t : workers) {
+        if (t.joinable()) t.join();
+    }
+}
